f78cdffa0c067a25cf0d7a4e785baf791edcedb5
root <root@localhost.localdomain>
2018-02-02 00:17:26 +0530

    Initial commit

---
+++ LinkedListDS/App.py
@@ -0,0 +1,14 @@
+from LinkedListDS.LinkedList import LinkedList
+
+linkedList = LinkedList();
+
+linkedList.insertStart(15)
+linkedList.insertEnd(122)
+linkedList.insertEnd(135)
+linkedList.insertStart(3)
+
+linkedList.traverseList()
+
+linkedList.remove(122)
+
+linkedList.traverseList()
